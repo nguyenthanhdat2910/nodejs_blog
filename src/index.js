1e63714b5d5a378b335cf703608a6253e52baf61
nguyenthanhdat <nguyenthanhdat291000@gmail.com>
2023-10-28 14:28:28 +0700

    Refactor code: create routes, app/controller folders

--- src/index.js
+++ src/index.js
@@ -5,10 +5,19 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // Template engine
 app.engine(
@@ -24,9 +33,8 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
