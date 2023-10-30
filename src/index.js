71fa99cc6fb76918061d05ec90d53deec7501645
nguyenthanhdat <nguyenthanhdat291000@gmail.com>
2023-10-31 00:26:19 +0700

    Delete course

--- src/index.js
+++ src/index.js
@@ -1,48 +1,48 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const { engine } = require('express-handlebars')
-const methodOverride = require('method-override')
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const methodOverride = require('method-override');
 
-const route = require('./routes')
-const db = require('./config/db')
+const route = require('./routes');
+const db = require('./config/db');
 
 // Connect to DB
-db.connect()
+db.connect();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
-)
-app.use(express.json())
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 // HTTP logger
 // app.use(morgan("combined"));
 
 // Template engine
 app.engine(
-  'hbs',
-  engine({
-    extname: '.hbs',
-    helpers: {
-      sum: (a, b) => a + b,
-    },
-  })
-)
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources', 'views'))
+    'hbs',
+    engine({
+        extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
-route(app)
+route(app);
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+    console.log(`App listening on port ${port}`);
+});
